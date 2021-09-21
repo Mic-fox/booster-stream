b9bdbfea27eeecebab225663148d6fcb02365831
Michelle Jacobs <michellejacobsza@gmail.com>
2021-09-22 00:20:40 +0200

    Login

--- src/Components/LoginModule.tsx
+++ src/Components/LoginModule.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useMoralis } from "react-moralis";
 
-const LoginModule = () => {
-    const { authenticate, isAuthenticated, user,web3 } = useMoralis();
 
+
+const LoginModule = () => {
+    const { authenticate, isAuthenticated, user  } = useMoralis();
     if (!isAuthenticated)  {
       return (
         <div>
@@ -11,12 +12,23 @@ const LoginModule = () => {
         </div>
       );
     }
-  
-    return (
-      <div>
-        <h1>Welcome {user?.get("username")}</h1>
-      </div>
-    );
+    console.log( user?.attributes)
+    if (user) {
+        return (
+            <div>
+              <h1>Welcome {user?.get("username")}</h1>
+              <span>{user?.attributes.ethAddress}</span>
+                <p>
+            
+                </p>
+            </div>
+          );
+    } else {
+        return (
+            <span>Loading</span>
+        )
+    }
+
 }
 
 export default LoginModule
