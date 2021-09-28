0f750cd597962494a9e9a6f9c452cdcfbaec5f4d
Michelle Jacobs <michellejacobsza@gmail.com>
2021-09-29 01:10:43 +0200

    Implemented ERC1155

--- packages/chain/contracts/SableTrove.sol
+++ packages/chain/contracts/SableTrove.sol
@@ -2,7 +2,8 @@
 pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-// TODO: implement ERC1155PresetMinterPauser
+import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
  
 contract SableTrove is ERC1155 {
     constructor(string memory uri_) ERC1155(uri_) {
